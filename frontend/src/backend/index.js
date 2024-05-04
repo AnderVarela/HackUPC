876b4ca7da839a3ad2d76bd5bba048f077e550bb
Ander Varela Martin <ander.varela@udc.es>
2024-05-05 01:33:25 +0200

    [FRONT] intento funcional de pantalla similares.

--- frontend/src/backend/index.js
+++ frontend/src/backend/index.js
@@ -1,15 +1,40 @@
+/*
 export default async function uploadImage(imageBase64) {
     try {
-        const response = await fetch("/uploadfile/", {
+        const response = await fetch("http://localhost:8000/uploadfile/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ image: imageBase64 })
+            body: JSON.stringify({filename: "a.jpg", filedata: imageBase64 })
         });
 
         if (!response.ok) {
             console.log("Error al enviar la imagen")
+            return null;
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error al subir la imagen:", error.message);
+        return null;
+    }
+}
+*/
+export default async function uploadImage(imageBase64, filename = "prueba.jpg") {
+    try {
+        const formData = new FormData();
+        formData.append('filename', filename);
+        formData.append('filedata', imageBase64);
+
+        const response = await fetch("http://localhost:8000/uploadfile/", {
+            method: "POST",
+            body: formData
+        });
+
+        if (!response.ok) {
+            console.log("Error al enviar la imagen");
             return null;
         }
 
